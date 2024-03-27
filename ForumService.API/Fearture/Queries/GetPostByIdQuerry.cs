5d043d291abc230f91bd9b53b839ea4182bedb94
HaVietChien24 <chienhaviet2408@gmail.com>
2024-03-28 00:38:15 +0700

    update code

--- ForumService.API/Fearture/Queries/GetPostByIdQuerry.cs
+++ ForumService.API/Fearture/Queries/GetPostByIdQuerry.cs
@@ -1,6 +1,7 @@
 ﻿using ForumService.API.Common.DTO;
+using ForumService.API.GrpcServices;
 using ForumService.API.Models;
-using GrpcServices;
+
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,8 +14,8 @@ namespace ForumService.API.Fearture.Queries
         public class GetPostIdQuerryHandler : IRequestHandler<GetPostByIdQuerry, PostDTO>
         {
             private readonly ForumContext _context;
-            private readonly GetUserPostGrpcService _service;
-            public GetPostIdQuerryHandler(ForumContext context,GetUserPostGrpcService service)
+            private readonly GetUserInfoService _service;
+            public GetPostIdQuerryHandler(ForumContext context, GetUserInfoService service)
             {
                 _context = context;
                 _service = service;
